b91eef783066a2f1376feee44a32103c825d7745
dange <dange0.tw@gmail.com>
2019-01-16 01:39:15 +0800

    Time per elements

--- access.c
+++ access.c
@@ -1,6 +1,7 @@
 #include "access.h"
 
-double tvgetf(){
+double tvgetf()
+{
     struct timespec ts;
     double sec;
 
@@ -9,38 +10,34 @@ double tvgetf(){
     sec /= 1e9;
     sec += ts.tv_sec;
 
-    return sec;
+    return sec * 1e9;
 }
 
-double sequence_access(unsigned int size){
-    int array[size];
-    int result;
-    int i;
-    int access_list[size];
-    for(i=0; i<size; i++){
-        access_list[i] = i;
+double sequence_access(unsigned int size)
+{
+    int array[size], index[size], result, i;
+    for (i = 0; i < size; i++) {
+        index[i] = i;
     }
 
     double t1 = tvgetf();
-    for(i=0; i<size; i++){
-        result += array[access_list[i]];
+    for (i = 0; i < size; i++) {
+        result += array[index[i]];
     }
     double t2 = tvgetf();
-    return t2 - t1;
+    return (t2 - t1) / size;
 }
 
-double random_access(unsigned int size){
-    int array[size];
-    int access_list[size];
-    int result;
-    int i;
-    for(i=0; i<(int)size; i++){
-        access_list[i] = rand() % size;
+double random_access(unsigned int size)
+{
+    int array[size], index[size], result, i;
+    for (i = 0; i < (int) size; i++) {
+        index[i] = rand() % size;
     }
     double t1 = tvgetf();
-    for(i=0; i<size; i++){
-        result += array[access_list[i]];
+    for (i = 0; i < size; i++) {
+        result += array[index[i]];
     }
     double t2 = tvgetf();
-    return t2 - t1;
+    return (t2 - t1) / size;
 }
--- main.c
+++ main.c
@@ -3,27 +3,28 @@
 #include <math.h>
 #include "access.h"
 
-#define MAX 30
+#define MAX 31
 
 
-int main(){
+int main()
+{
     double result[MAX][2];
     unsigned int size = 0;
-    for(int i = 0; i < MAX; i++){
-        size = (unsigned int)pow(2,i);
+    for (int i = 10; i < MAX; i++) {
+        size = (unsigned int) pow(2, i);
         result[i][0] = sequence_access(size);
         result[i][1] = random_access(size);
     }
-    
+
     FILE *f_seq, *f_ram;
-    f_seq = fopen("result_seq.txt","w");
-    f_ram = fopen("result_ram.txt","w");
-    
-    for(int i = 0; i < MAX; i++){
+    f_seq = fopen("result_seq.txt", "w");
+    f_ram = fopen("result_ram.txt", "w");
+
+    for (int i = 10; i < MAX; i++) {
         fprintf(f_seq, "%d %lf\n", i, result[i][0]);
         fprintf(f_ram, "%d %lf\n", i, result[i][1]);
     }
-    
+
     fclose(f_seq);
     fclose(f_ram);
     return 0;
